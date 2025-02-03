ec5aa5633209a63139e761b75b495a7e4e6b39a0
Huizerd <15855769+Huizerd@users.noreply.github.com>
2025-02-03 11:23:21 +0100

    revert external modes back to none else depth seeker doesn't work

--- src/modules/flight_mode_manager/FlightModeManager.cpp
+++ src/modules/flight_mode_manager/FlightModeManager.cpp
@@ -136,7 +136,10 @@ void FlightModeManager::updateParams()
 void FlightModeManager::start_flight_task()
 {
 	// Do not run any flight task for VTOLs in fixed-wing mode
-	if (_vehicle_status_sub.get().vehicle_type == vehicle_status_s::VEHICLE_TYPE_FIXED_WING) {
+	// external states also return none and don't require/handle manual input
+	if ((_vehicle_status_sub.get().vehicle_type == vehicle_status_s::VEHICLE_TYPE_FIXED_WING)
+	    || ((_vehicle_status_sub.get().nav_state >= vehicle_status_s::NAVIGATION_STATE_EXTERNAL1)
+		&& (_vehicle_status_sub.get().nav_state <= vehicle_status_s::NAVIGATION_STATE_EXTERNAL8))) {
 		switchTask(FlightTaskIndex::None);
 		return;
 	}
@@ -202,11 +205,7 @@ void FlightModeManager::start_flight_task()
 	}
 
 	// Manual position control
-	// also switch all external modes to position control instead of 'none task'
-	if ((_vehicle_status_sub.get().nav_state == vehicle_status_s::NAVIGATION_STATE_POSCTL)
-	    || ((_vehicle_status_sub.get().nav_state >= vehicle_status_s::NAVIGATION_STATE_EXTERNAL1)
-		    && (_vehicle_status_sub.get().nav_state <= vehicle_status_s::NAVIGATION_STATE_EXTERNAL8))
-		|| task_failure) {
+	if ((_vehicle_status_sub.get().nav_state == vehicle_status_s::NAVIGATION_STATE_POSCTL) || task_failure) {
 		found_some_task = true;
 		FlightTaskError error = FlightTaskError::NoError;
 
