81749cf265e7962993aa60755e18c23fc80fc9d4
sstroobants <stein.stroobants@live.nl>
2025-02-04 17:02:47 +0100

    set priority to be relative to PRIORITY_MAX

--- src/modules/uxrce_dds_client/uxrce_dds_client.cpp
+++ src/modules/uxrce_dds_client/uxrce_dds_client.cpp
@@ -753,7 +753,7 @@ int UxrceddsClient::task_spawn(int argc, char *argv[])
 {
 	_task_id = px4_task_spawn_cmd("uxrce_dds_client",
 				      SCHED_DEFAULT,
-				      200,
+				      SCHED_PRIORITY_MAX - 55,
 				      PX4_STACK_ADJUSTED(12000),
 				      (px4_main_t)&run_trampoline,
 				      (char *const *)argv);
